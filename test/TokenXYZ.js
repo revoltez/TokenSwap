5660197d23422b854000403a7e06c6bae909420c
Ego_Renegade <eziorevoltez@gmail.com>
2021-11-02 15:08:44 +0100

    added TokenXYZ test

--- test/TokenXYZ.js
+++ test/TokenXYZ.js
@@ -1,3 +1,9 @@
 const TokenXYZ = artifacts.require("./TokenXYZ.sol");
 
-contract("TokenXYZ", (accounts) => {});
+contract("TokenXYZ", (accounts) => {
+  it("Testing the XYZ Token price", async () => {
+    const TokenInstance = await TokenXYZ.deployed();
+    const result = await TokenInstance.totalSupply.call();
+    assert.equal(1000000, result);
+  });
+});
